483fa2ff49dfa1fd8f8de8a038891a214db7cd69
Rolf Krahl <rolf@rotkraut.de>
2019-02-25 20:52:34 +0100

    - Merge existing scripts into one unified command line interface. - Add verify subcommand to the cli.

---
+++ scripts/archive-tool.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python
+
+import argparse
+from pathlib import Path
+from archive import Archive
+
+def create(args):
+    if args.compression == 'none':
+        args.compression = ''
+    mode = 'x:%s' % args.compression
+    archive = Archive(args.archive, mode, args.files, args.basedir)
+
+def ls(args):
+    archive = Archive(args.archive, "r")
+    items = []
+    l_ug = 0
+    l_s = 0
+    for fi in archive.manifest:
+        elems = tuple(str(fi).split("  "))
+        l_ug = max(l_ug, len(elems[1]))
+        l_s = max(l_s, len(elems[2]))
+        items.append(elems)
+    format_str = "%%s  %%%ds  %%%ds  %%s  %%s" % (l_ug, l_s)
+    for i in items:
+        print(format_str % i)
+
+def verify(args):
+    archive = Archive(args.archive, "r")
+    archive.verify()
+
+argparser = argparse.ArgumentParser()
+subparsers = argparser.add_subparsers(title='subcommands')
+
+create_parser = subparsers.add_parser('create', help="create the archive")
+create_parser.add_argument('archive',
+                           help=("path to the archive file"), type=Path)
+create_parser.add_argument('--compression',
+                           choices=['none', 'gz', 'bz2', 'xz'], default='gz',
+                           help=("compression mode"))
+create_parser.add_argument('--basedir',
+                           help=("common base directory in the archive"))
+create_parser.add_argument('files', nargs='+', type=Path,
+                           help="files to add to the archive")
+create_parser.set_defaults(func=create)
+
+ls_parser = subparsers.add_parser('ls', help="list files in the archive")
+ls_parser.add_argument('archive',
+                       help=("path to the archive file"), type=Path)
+ls_parser.set_defaults(func=ls)
+
+verify_parser = subparsers.add_parser('verify',
+                                      help="verify integrity of the archive")
+verify_parser.add_argument('archive',
+                           help=("path to the archive file"), type=Path)
+verify_parser.set_defaults(func=verify)
+
+args = argparser.parse_args()
+if not hasattr(args, "func"):
+    argparser.error("subcommand is required")
+args.func(args)
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     license = "Apache-2.0",
     requires = ["PyYAML"],
     packages = ["archive"],
-    scripts = ["scripts/create-archive.py", "scripts/ls-archive.py"],
+    scripts = ["scripts/archive-tool.py"],
     classifiers = [
         "Development Status :: 1 - Planning",
         "Intended Audience :: System Administrators",
