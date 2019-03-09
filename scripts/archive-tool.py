ba1b8a83a8fdad73327526dfc4ae24ccf60e05fc
Rolf Krahl <rolf@rotkraut.de>
2019-03-09 15:27:46 +0100

    Reorder some things in the archive-tool.py script.

--- scripts/archive-tool.py
+++ scripts/archive-tool.py
@@ -4,11 +4,39 @@ import argparse
 from pathlib import Path
 from archive import Archive
 
+argparser = argparse.ArgumentParser()
+subparsers = argparser.add_subparsers(title='subcommands')
+
+
 def create(args):
     if args.compression == 'none':
         args.compression = ''
     mode = 'x:%s' % args.compression
     archive = Archive(args.archive, mode, args.files, args.basedir)
+
+create_parser = subparsers.add_parser('create', help="create the archive")
+create_parser.add_argument('--compression',
+                           choices=['none', 'gz', 'bz2', 'xz'], default='gz',
+                           help=("compression mode"))
+create_parser.add_argument('--basedir',
+                           help=("common base directory in the archive"))
+create_parser.add_argument('archive',
+                           help=("path to the archive file"), type=Path)
+create_parser.add_argument('files', nargs='+', type=Path,
+                           help="files to add to the archive")
+create_parser.set_defaults(func=create)
+
+
+def verify(args):
+    archive = Archive(args.archive, "r")
+    archive.verify()
+
+verify_parser = subparsers.add_parser('verify',
+                                      help="verify integrity of the archive")
+verify_parser.add_argument('archive',
+                           help=("path to the archive file"), type=Path)
+verify_parser.set_defaults(func=verify)
+
 
 def ls(args):
     archive = Archive(args.archive, "r")
@@ -24,35 +52,11 @@ def ls(args):
     for i in items:
         print(format_str % i)
 
-def verify(args):
-    archive = Archive(args.archive, "r")
-    archive.verify()
-
-argparser = argparse.ArgumentParser()
-subparsers = argparser.add_subparsers(title='subcommands')
-
-create_parser = subparsers.add_parser('create', help="create the archive")
-create_parser.add_argument('archive',
-                           help=("path to the archive file"), type=Path)
-create_parser.add_argument('--compression',
-                           choices=['none', 'gz', 'bz2', 'xz'], default='gz',
-                           help=("compression mode"))
-create_parser.add_argument('--basedir',
-                           help=("common base directory in the archive"))
-create_parser.add_argument('files', nargs='+', type=Path,
-                           help="files to add to the archive")
-create_parser.set_defaults(func=create)
-
 ls_parser = subparsers.add_parser('ls', help="list files in the archive")
 ls_parser.add_argument('archive',
                        help=("path to the archive file"), type=Path)
 ls_parser.set_defaults(func=ls)
 
-verify_parser = subparsers.add_parser('verify',
-                                      help="verify integrity of the archive")
-verify_parser.add_argument('archive',
-                           help=("path to the archive file"), type=Path)
-verify_parser.set_defaults(func=verify)
 
 args = argparser.parse_args()
 if not hasattr(args, "func"):
