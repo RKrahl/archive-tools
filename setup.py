91a6a9fe9067507badfb062cd9667d5a61ec66ec
Rolf Krahl <rolf@rotkraut.de>
2019-03-10 14:24:01 +0100

    Add a header section to the manifest.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author = AUTHOR_NAME,
     author_email = AUTHOR_EMAIL,
     license = "Apache-2.0",
-    requires = ["PyYAML"],
+    requires = ["PyYAML", "dateutil"],
     packages = ["archive"],
     scripts = ["scripts/create-archive.py", "scripts/ls-archive.py"],
     classifiers = [
