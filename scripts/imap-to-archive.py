11d1a5bca7da928558df4ba9fc3f8f11338f9e0c
Rolf Krahl <rolf@rotkraut.de>
2019-07-07 20:47:20 +0200

    Make imap-to-archive.py configurable.

--- scripts/imap-to-archive.py
+++ scripts/imap-to-archive.py
@@ -3,33 +3,95 @@
 """
 
 import argparse
+from collections import ChainMap
+import configparser
 import getpass
 import logging
+import os.path
 from pathlib import Path
+import sys
 from imapclient import IMAPClient
 from archive.mailarchive import MailArchive
 from archive.tools import now_str
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
-
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logging.getLogger('imapclient').setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
 
-# FIXME: should have a more decent configuration:
-# - There should be a configuration file, so that not everything need
-#   to be set on the command line.
-# - Several things are still hard coded in the script that should be
-#   configurable.
+security_methods = {'imaps', 'starttls'}
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('host',
+default_config_file = os.path.expanduser("~/.config/archive/imap.cfg")
+defaults = {
+    'host': None,
+    'port': None,
+    'security': 'imaps',
+    'user': None,
+    'pass': None,
+}
+
+argparser = argparse.ArgumentParser(add_help=False)
+argparser.add_argument('--help',
+                       action='help', default=argparse.SUPPRESS,
+                       help=('show this help message and exit'))
+argparser.add_argument('-c', '--config-file', default=default_config_file,
+                       help=("configuration file"))
+argparser.add_argument('-s', '--config-section',
+                       help=("section in the configuration file"))
+argparser.add_argument('-h', '--host',
                        help=("host name of the IMAP4 server"))
-argparser.add_argument('user',
+argparser.add_argument('-p', '--port', type=int,
+                       help=("port of the IMAP4 server"))
+argparser.add_argument('--security', choices=security_methods,
+                       help=("security method"))
+argparser.add_argument('-u', '--user',
                        help=("IMAP4 user name"))
-argparser.add_argument('archive',
-                       help=("path to the archive file"), type=Path)
+argparser.add_argument('-v', '--verbose', action='store_true',
+                       help=("verbose diagnostic output"))
+argparser.add_argument('archive', type=Path,
+                       help=("path to the archive file"))
 args = argparser.parse_args()
 
-log = logging.getLogger(__name__)
+if args.verbose:
+    logging.getLogger().setLevel(logging.DEBUG)
+
+class ConfigError(Exception):
+    pass
+
+def get_config(args, defaults):
+    args_cfg_options = ('host', 'port', 'security', 'user')
+    args_cfg = { k:vars(args)[k] for k in args_cfg_options if vars(args)[k] }
+    config = ChainMap({}, args_cfg)
+    if args.config_section:
+        cp = configparser.ConfigParser()
+        if not cp.read(args.config_file):
+            raise ConfigError("configuration file %s not found"
+                              % args.config_file)
+        try:
+            config.maps.append(cp[args.config_section])
+        except KeyError:
+            raise ConfigError("configuration section %s not found"
+                              % args.config_section)
+    config.maps.append(defaults)
+
+    if config['security'] not in security_methods:
+        raise ConfigError("invalid security method '%s'" % config['security'])
+    if not config['host']:
+        raise ConfigError("IMAP4 host name not specified")
+    if config['port'] is not None:
+        config['port'] = int(config['port'])
+    config['ssl'] = config['security'] == 'imaps'
+    if not config['user']:
+        raise ConfigError("IMAP4 user name not specified")
+    if config['pass'] is None:
+        config['pass'] = getpass.getpass()
+
+    return config
+
+try:
+    config = get_config(args, defaults)
+except ConfigError as e:
+    print("%s: configuration error: %s" % (argparser.prog, e), file=sys.stderr)
+    sys.exit(2)
 
 
 def getmsgs(imap, basedir):
@@ -58,11 +120,12 @@ def getmsgs(imap, basedir):
 
 
 archive_path = Path.cwd() / args.archive
-with IMAPClient(args.host, ssl=False) as imap:
-    imap.starttls()
-    imap.login(args.user, getpass.getpass())
-    log.debug("Login to %s successful", args.host)
-    comment = "Fetched from %s at %s" % (args.host, now_str())
+with IMAPClient(config['host'], port=config['port'], ssl=config['ssl']) as imap:
+    if config['security'] == 'starttls':
+        imap.starttls()
+    imap.login(config['user'], config['pass'])
+    log.debug("Login to %s successful", config['host'])
+    comment = "Fetched from %s at %s" % (config['host'], now_str())
     archive = MailArchive()
     archive.create(archive_path, getmsgs(imap, "INBOX"), comment=comment)
 
