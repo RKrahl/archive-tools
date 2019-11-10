56110d55403001a9bc01a1b90cb766585817921c
Rolf Krahl <rolf@rotkraut.de>
2019-11-10 22:30:33 +0100

    Add a list of subcommands.

--- archive/cli/__init__.py
+++ archive/cli/__init__.py
@@ -1,17 +1,12 @@
 """Provide the subcommands of the archive-tool command line tool.
 """
 
-import archive.cli.create
-import archive.cli.verify
-import archive.cli.ls
-import archive.cli.info
-import archive.cli.check
+import importlib
 
+subcmds = [ "create", "verify", "ls", "info", "check", ]
 
 def add_subparsers(argparser):
     subparsers = argparser.add_subparsers(title='subcommands', dest='subcmd')
-    archive.cli.create.add_parser(subparsers)
-    archive.cli.verify.add_parser(subparsers)
-    archive.cli.ls.add_parser(subparsers)
-    archive.cli.info.add_parser(subparsers)
-    archive.cli.check.add_parser(subparsers)
+    for sc in subcmds:
+        m = importlib.import_module('archive.cli.%s' % sc)
+        m.add_parser(subparsers)
