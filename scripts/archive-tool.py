935f27745e47ac5999ed3f2902d191fde4b6e9a6
Rolf Krahl <rolf@rotkraut.de>
2019-11-12 20:22:23 +0100

    Move the remaining bits of archive-tool.py into package archive.cli.

--- scripts/archive-tool.py
+++ scripts/archive-tool.py
@@ -1,53 +1,5 @@
 #! /usr/bin/python
 
-import argparse
-import warnings
-import sys
 import archive.cli
-from archive.exception import *
 
-
-def showwarning(message, category, filename, lineno, file=None, line=None):
-    """Display ArchiveWarning in a somewhat more user friendly manner.
-    All other warnings are formatted the standard way.
-    """
-    # This is a modified version of the function of the same name from
-    # the Python standard library warnings module.
-    if file is None:
-        file = sys.stderr
-        if file is None:
-            # sys.stderr is None when run with pythonw.exe - warnings get lost
-            return
-    try:
-        if issubclass(category, ArchiveWarning):
-            s = "%s: %s\n" % (argparser.prog, message)
-        else:
-            s = warnings.formatwarning(message, category, 
-                                       filename, lineno, line)
-        file.write(s)
-    except OSError:
-        pass # the file (probably stderr) is invalid - this warning gets lost.
-warnings.showwarning = showwarning
-
-
-argparser = argparse.ArgumentParser()
-archive.cli.add_subparsers(argparser)
-args = argparser.parse_args()
-if not hasattr(args, "func"):
-    argparser.error("subcommand is required")
-try:
-    args.func(args)
-except ArgError as e:
-    argparser.error(str(e))
-except ArchiveError as e:
-    if isinstance(e, ArchiveCreateError):
-        status = 1
-    elif isinstance(e, ArchiveReadError):
-        status = 1
-    elif isinstance(e, ArchiveIntegrityError):
-        status = 3
-    else:
-        raise
-    print("%s %s: error: %s" % (argparser.prog, args.subcmd, e), 
-          file=sys.stderr)
-    sys.exit(status)
+archive.cli.archive_tool()
