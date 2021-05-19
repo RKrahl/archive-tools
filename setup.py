4ed3276969cd91a99696f086aa726db14645d244
Rolf Krahl <rolf@rotkraut.de>
2021-05-19 16:59:39 +0200

    Formally drop support for Python 3.4 and Python 3.5

--- setup.py
+++ setup.py
@@ -126,8 +126,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
@@ -136,4 +134,3 @@ setup(
         ],
     cmdclass = {'build_py': build_py, 'sdist': sdist, 'init_py': init_py},
 )
-
