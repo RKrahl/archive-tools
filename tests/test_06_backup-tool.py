b6e7ef4fe8b94ba865b41097e7eaf7746ad8fd2e
Rolf Krahl <rolf@rotkraut.de>
2021-10-10 23:21:39 +0200

    Move backup-tool into a test class

--- tests/test_06_backup-tool.py
+++ tests/test_06_backup-tool.py
@@ -22,6 +22,7 @@ class BTTestEnv:
 
     def __init__(self, root):
         self.root = root
+        self.root.mkdir()
         self.monkeypatch = MonkeyPatch()
         self._datetime = FrozenDateTime
         self._date = FrozenDate
@@ -79,12 +80,14 @@ class BTTestEnv:
             backup_tool()
         assert excinfo.value.code == 0
 
-@pytest.fixture(scope="module")
-def env(tmpdir):
-    with BTTestEnv(tmpdir) as e:
+@pytest.fixture(scope="class")
+def env(tmpdir, request):
+    with BTTestEnv(tmpdir / request.cls.__name__) as e:
         yield e
 
-cfg = """# Configuration file for backup-tool.
+class TestBackupTool:
+
+    cfg = """# Configuration file for backup-tool.
 # All paths are within a root directory that need to be substituted.
 
 [DEFAULT]
@@ -130,64 +133,67 @@ schedule.cumu.date = Mon *
 schedule.incr.date = *
 """
 
-def test_backup(env):
-    subst = dict(root=env.root)
-    cfg_data = string.Template(cfg).substitute(subst).encode('ascii')
-    cfg_path = Path("etc", "backup.cfg")
-    sys_data = [
-        DataDir(Path("etc"), 0o755, mtime=1633129414),
-        DataContentFile(cfg_path, cfg_data, 0o644, mtime=1632596683),
-        DataContentFile(Path("etc", "foo.cfg"),
-                        b"[foo]\nbar = baz\n", 0o644, mtime=1632672000),
-        DataDir(Path("root"), 0o700, mtime=1633274230),
-        DataRandomFile(Path("root", "rnd5.dat"),
-                       0o600, size=85, mtime=1633243020),
-        DataSymLink(Path("root", "rnd.dat"), Path("rnd5.dat"),
-                    mtime=1633243020),
-    ]
-    env.add_test_data(('desk','serv'), sys_data)
-    sys_serv_data = [
-        DataDir(Path("usr", "local"), 0o755, mtime=1616490893),
-        DataRandomFile(Path("usr", "local", "rnd6.dat"),
-                       0o644, size=607, mtime=1633275272),
-    ]
-    env.add_test_data(('serv',), sys_serv_data)
-    user_data = [
-        DataDir(Path("home", "jdoe"), 0o700, mtime=1633263300),
-        DataRandomFile(Path("home", "jdoe", "rnd3.dat"),
-                       0o600, size=796, mtime=1633243020),
-    ]
-    env.add_test_data(('user',), user_data)
-    excl_data = [
-        DataDir(Path("home", "jdoe", ".cache"), 0o700, mtime=1608491257),
-        DataRandomFile(Path("home", "jdoe", ".cache", "rnd2.dat"),
-                       0o600, size=385, mtime=1633275272),
-        DataDir(Path("home", "jdoe", "tmp"), 0o755, mtime=1631130997),
-        DataDir(Path("root", ".cache"), 0o700, mtime=1603009887),
-        DataRandomFile(Path("root", ".cache", "rnd4.dat"),
-                       0o600, size=665, mtime=1633275272),
-        DataDir(Path("net", "backup"), 0o755, mtime=1632704400),
-        DataDir(Path("var", "backup"), 0o755, mtime=1632704400),
-    ]
-    env.add_test_data(('excl',), excl_data)
-    env.setup_test_data()
-    env.monkeypatch.setenv("BACKUP_CFG", str(env.root / cfg_path))
+    def init_data(self, env):
+        subst = dict(root=env.root)
+        cfg_data = string.Template(self.cfg).substitute(subst).encode('ascii')
+        cfg_path = Path("etc", "backup.cfg")
+        sys_data = [
+            DataDir(Path("etc"), 0o755, mtime=1633129414),
+            DataContentFile(cfg_path, cfg_data, 0o644, mtime=1632596683),
+            DataContentFile(Path("etc", "foo.cfg"),
+                            b"[foo]\nbar = baz\n", 0o644, mtime=1632672000),
+            DataDir(Path("root"), 0o700, mtime=1633274230),
+            DataRandomFile(Path("root", "rnd5.dat"),
+                           0o600, size=85, mtime=1633243020),
+            DataSymLink(Path("root", "rnd.dat"), Path("rnd5.dat"),
+                        mtime=1633243020),
+        ]
+        env.add_test_data(('desk','serv'), sys_data)
+        sys_serv_data = [
+            DataDir(Path("usr", "local"), 0o755, mtime=1616490893),
+            DataRandomFile(Path("usr", "local", "rnd6.dat"),
+                           0o644, size=607, mtime=1633275272),
+        ]
+        env.add_test_data(('serv',), sys_serv_data)
+        user_data = [
+            DataDir(Path("home", "jdoe"), 0o700, mtime=1633263300),
+            DataRandomFile(Path("home", "jdoe", "rnd3.dat"),
+                           0o600, size=796, mtime=1633243020),
+        ]
+        env.add_test_data(('user',), user_data)
+        excl_data = [
+            DataDir(Path("home", "jdoe", ".cache"), 0o700, mtime=1608491257),
+            DataRandomFile(Path("home", "jdoe", ".cache", "rnd2.dat"),
+                           0o600, size=385, mtime=1633275272),
+            DataDir(Path("home", "jdoe", "tmp"), 0o755, mtime=1631130997),
+            DataDir(Path("root", ".cache"), 0o700, mtime=1603009887),
+            DataRandomFile(Path("root", ".cache", "rnd4.dat"),
+                           0o600, size=665, mtime=1633275272),
+            DataDir(Path("net", "backup"), 0o755, mtime=1632704400),
+            DataDir(Path("var", "backup"), 0o755, mtime=1632704400),
+        ]
+        env.add_test_data(('excl',), excl_data)
+        env.setup_test_data()
+        env.monkeypatch.setenv("BACKUP_CFG", str(env.root / cfg_path))
 
-    env.set_hostname("desk")
-    env.set_datetime(datetime.datetime(2021, 10, 3, 19, 30))
-    env.run_backup_tool("backup-tool --verbose create --policy sys")
-    path = env.root / "var" / "backup" / "desk-211003-full.tar.bz2"
-    env.check_archive(path, 'desk', 'full')
-    path.rename(env.root / "net" / "backup" / "desk-211003-full.tar.bz2")
+    def test_initial_full_backup(self, env):
+        self.init_data(env)
 
-    env.set_hostname("serv")
-    env.set_datetime(datetime.datetime(2021, 10, 4, 3, 0))
-    env.run_backup_tool("backup-tool --verbose create --policy sys")
-    path = env.root / "net" / "backup" / "serv-211004-full.tar.bz2"
-    env.check_archive(path, 'serv', 'full')
-    env.run_backup_tool("backup-tool --verbose create --user jdoe")
-    path = env.root / "net" / "backup" / "jdoe-211004-full.tar.bz2"
-    env.check_archive(path, 'user', 'full')
-    env.run_backup_tool("backup-tool --verbose index")
+        env.set_hostname("desk")
+        env.set_datetime(datetime.datetime(2021, 10, 3, 19, 30))
+        env.run_backup_tool("backup-tool --verbose create --policy sys")
+        path = env.root / "var" / "backup" / "desk-211003-full.tar.bz2"
+        env.check_archive(path, 'desk', 'full')
+        path.rename(env.root / "net" / "backup" / "desk-211003-full.tar.bz2")
 
-    env.flush_test_data(('desk', 'serv', 'user'), 'cumu')
+        env.set_hostname("serv")
+        env.set_datetime(datetime.datetime(2021, 10, 4, 3, 0))
+        env.run_backup_tool("backup-tool --verbose create --policy sys")
+        path = env.root / "net" / "backup" / "serv-211004-full.tar.bz2"
+        env.check_archive(path, 'serv', 'full')
+        env.run_backup_tool("backup-tool --verbose create --user jdoe")
+        path = env.root / "net" / "backup" / "jdoe-211004-full.tar.bz2"
+        env.check_archive(path, 'user', 'full')
+        env.run_backup_tool("backup-tool --verbose index")
+
+        env.flush_test_data(('desk', 'serv', 'user'), 'cumu')
